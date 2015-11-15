c3308452b6066b59960818b745d10a81ec01bb19
HwangJunwoo <capollux10@gmail.com>
2015-11-15 11:41:00 +0900

    Make controllers And Setting
    - Home, Post, Reply controllers
    - Setting routes
    - Setting default_url_option, time_zone
    - Add some pictures
    - CRUD

---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post < ActiveRecord::Base
+
+	belongs_to :user
+
+	has_many :replies
+
+	validates :title, :presence => true
+	validates :sub_title, :presence => true
+	validates :context, :presence => true
+
+end
