1de34220e472766a7697fdd66dbd59ecca9d30cf
Hwang Junwoo <capollux10@gmail.com>
2016-04-07 02:45:17 +0900

    Fix update password bus

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
 
 		def configure_permitted_parameters
 			devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :admin) }
-			devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :profile, :email, :password, :password_confirmation) }
+			devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :profile, :email, :password, :password_confirmation, :current_password) }
 		end
 		
 end
